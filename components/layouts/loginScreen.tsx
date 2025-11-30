6f32ea00580ac1a309d511f739b3d2ef2b08080a
Alfredo <alf.alvarezr@duocuc.cl>
2025-11-30 18:52:57 -0300

    fix: Puerto restaurado

--- components/layouts/loginScreen.tsx
+++ components/layouts/loginScreen.tsx
@@ -29,7 +29,7 @@ const LoginScreen = () => {
 
         
         try {
-            const response = await fetch("http://localhost:3000/api/auth/login", {
+            const response = await fetch("http://localhost:8080/api/auth/login", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
